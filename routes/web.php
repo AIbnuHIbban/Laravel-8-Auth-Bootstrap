91d9ee717f77ac8d43323b9d5c7478aaad577568
aibnuhibban <abd.ibnuhibban@gmail.com>
2020-10-16 10:39:43 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
